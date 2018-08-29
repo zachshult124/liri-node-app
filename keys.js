29196318c82d0ad2e53c2f7f2476dc1e2f1038a9
zachshult <zachshult12345@gmail.com>
2018-08-29 13:26:13 -0500

    more code

--- keys.js
+++ keys.js
@@ -1,6 +1,10 @@
-console.log('loaded like a baked potatoe')
+console.log('keys loaded like a baked potatoe')
 
 exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
+
+exports.omdb = {
+    API_KEY: process.env.BANDSINTOWN_ID
+}
--- liri.js
+++ liri.js
@@ -1,5 +1,133 @@
+// require("dotenv").config();
+
+// var request = require('request');
+// var keys = require("./keys.js");
+// var request = require('request');
+// var inputString = process.argv
+// var userInput1 = inputString[2];
+// var userInput2 = inputString.slice(3).join(' ');
+
+
+// //-------------------Spotify--------------------------//
+// //Spotify requests
+// var Spotify = require('node-spotify-api');
+// var spotify = new Spotify(keys.spotify);
+
+// //functions created for search inputs
+// /////bandsintown//////
+// var queryURL = "https://rest.bandsintown.com/artists/" + "drake" + "/events?app_id=" + keys.bandsintown.api_key;
+
+
+// request(queryURL, function (error, response, body) {
+//     console.log('error:', error); // Print the error if one occurred
+//     console.log('statusCode:', response && response.statusCode);
+
+//     // Print the response status code if a response was received
+//     console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
+// });
+
+
+
+
+
+// function spotifySearch() {
+//     spotify.search({ type: 'track', query: userInput2, limit: 1 }, function (err, data) {
+//         if (err) {
+//             return console.log('Error occurred: ' + err);
+//         }
+//         console.log(data);
+//     });
+// };
+
+
+
+
+
+
+
+
+
+// function movieSearch() {
+//     console.log('movie');
+// };
+
+
+
+
+// function doWhatItSays() {
+//     console.log('do what it says');
+// };
+
+// var search = function () {
+//     if (userInput1 === 'concert-this') {
+//         concertSearch(userInput2);
+//     } else if (userInput1 === 'spotify-this-song') {
+//         spotifySearch(userInput2);
+//     } else if (userInput1 === 'movie-this') {
+//         movieSearch(userInput2);
+//     } else if (userInput1 === 'do-what-it-says') {
+//         doWhatItSays(userInput2);
+//     } else {
+//         console.log('something')
+//     };
+// };
+
+// //Function call that begins script
+// search(inputString);
+
+
 require("dotenv").config();
 
-var keys = require('./keys.js')
+var keys = require("./keys.js");
+var request = require('request');
+var inputString = process.argv;
+var userInput1 = inputString[2];
+var userInput2 = inputString.slice(3).join(' ');
 
+//Spotify requests
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
+//functions created for search inputs
+function concertSearch() {
+    var BANDSINTOWN_QueryURL = "https://rest.bandsintown.com/artists/" + userInput2 + "/events?app_id=" + keys.bandsInTown.BANDSINTOWN_ID;
+    request(BANDSINTOWN_QueryURL, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode);
+
+        // Print the response status code if a response was received
+        console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
+    });
+};
+
+function spotifySearch() {
+    spotify.search({ type: 'track', query: userInput2, limit: 1 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(data);
+    });
+};
+
+function movieSearch() {
+    console.log('movie');
+};
+
+function doWhatItSays() {
+    console.log('do what it says');
+};
+
+var search = function () {
+    if (userInput1 === 'concert-this') {
+        concertSearch(userInput2);
+    } else if (userInput1 === 'spotify-this-song') {
+        spotifySearch(userInput2);
+    } else if (userInput1 === 'movie-this') {
+        movieSearch(userInput2);
+    } else if (userInput1 === 'do-what-it-says') {
+        doWhatItSays(userInput2);
+    } else { };
+};
+
+//Function call that begins script
+search(userInput2);
